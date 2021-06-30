690e8d7cd4175adda10031ab7bb52fb8b9724083
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-06-30 14:57:45 +0800

    dojo dashboard 3

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -8,6 +8,31 @@ class DojosController < ApplicationController
   end
 
   def show
+    # fail
+    @dojo = Dojo.find(params[:id])
+  end
+
+  def edit
+    @dojo = Dojo.find(params[:id])
+  end
+
+  def update
+    @dojo = Dojo.find(params[:id])
+    @dojo.update(user_params)
+    if @dojo.valid?
+      flash[:notice] = "Dojo data has been updated successfully"
+    else
+      flash[:alert] = @dojo.errors.full_messages
+    end
+    redirect_to "/dojos"
+  end
+
+  def delete
+    dojo = Dojo.find(params[:id])
+    dojo.destroy
+    flash[:notice] = "Dojo data has been deleted successfully"
+    redirect_to "/dojos"
+  end
 
   def create
     @dojo = Dojo.create(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   get 'dojos/index'
   get 'dojos/new'
   post 'dojos/create'
-
-  get 'dojos/:id'
   get 'dojos' => 'dojos#index'
+  get 'dojos/:id' => "dojos#show"
+
+  get 'dojos/:id/edit' => 'dojos#edit'
+
+  patch 'dojos/:id' => 'dojos#update'
+  delete 'dojos/:id' => 'dojos#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
